dfb389d30ed8318f1d62e95e18eef82867c5f895
IvanKoDev <50876196+IvanKoDev@users.noreply.github.com>
2019-10-14 19:25:51 +0300

    Update 3.js

--- lesson-7/3.js
+++ lesson-7/3.js
@@ -11,9 +11,47 @@
  */
 
 // Решение
+//==================================================================
+
+// фция для проверки параметров на соответствие бизнес-условиям
+const validator = function(value, length) {
+
+	// проверка параметра value на соответствие заданным типам данных
+	if (!(typeof value === 'number') && !(typeof value === 'string') && !(typeof value === 'object')) {
+		if (!Array.isArray(value)) {
+
+			throw new Error('wrong type of new array element!');
+		}
+	}
+	
+	// проверка параметра length на соответствие типу number
+	if (typeof length !== 'number') {
+
+		throw new Error('the length of new array is not a number!');
+	}
+};
+
+const createArray = function(value, length) {
+
+	validator(value, length);
+
+	let arr = new Array (length); 		// создаем массив пустых элементов с длиной length
+	arr.fill(value);					// заполняем массив значениями value
+	
+	return arr;							// возвращаем созданный массив
+	
+	// return Array(length).fill(value) // короткий вариант
+};
+
+//--------------------------------------------------------------------
 
 const result = createArray('x', 5);
-
 console.log(result); // [ x, x, x, x, x ]
 
-exports.createArray = createArray;
+const result1 = createArray(undefined, 3);
+console.log(result1); // Error: wrong type of new array element!
+
+const result2 = createArray('x', '5');
+console.log(result2); // Error: the length of new array is not a number!
+
+// exports.createArray = createArray;
