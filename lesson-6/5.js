78eb6ae43ab8ab1a54fbf9f07b58fa6dd1d611c2
IvanKoDev <50876196+IvanKoDev@users.noreply.github.com>
2019-10-16 03:15:46 +0300

    Update 5.js

--- lesson-6/5.js
+++ lesson-6/5.js
@@ -17,20 +17,54 @@
  */
 
 const array = [1, 2, 3, 4, 5];
+// const array = [];
 const INITIAL_ACCUMULATOR = 6;
 
 // Решение
 
+const validator = function(arrayRef, callback) {
+	// проверка первого аргумента на принадлежность к массивам
+	if (!Array.isArray(arrayRef)) {
+		throw new Error('first argument is not an array');
+	}
+
+	// проверка второго аргумента на принадлежность к функциям
+	if (typeof callback !== 'function') {
+		throw new Error('second argument is not a function');
+	}
+};
+
+const reduce = function(arrayRef, callback, INITIAL_ACCUMULATOR) {
+    
+	validator(arrayRef, callback);
+
+    // задаем начальное значение аккумулятора для callback-фции согласно бизнес-условиям
+    let accumulator = INITIAL_ACCUMULATOR ? INITIAL_ACCUMULATOR : 0;
+    
+    // возвращаем INITIAL_ACCUMULATOR если массив пустой
+    if (arrayRef.length === 0) {
+		return INITIAL_ACCUMULATOR;
+	};    
+
+    // пропускаем целевой массив через collback-фцию и накапливаем результат в аккумулятор
+    for (let i = 0; i < arrayRef.length; i++) {
+
+        accumulator = callback(accumulator, arrayRef[i], i, arrayRef);
+	}
+
+    return accumulator;
+};
+
 const result = reduce(
-    array,
-    (accumulator, element, index, arrayRef) => {
-        console.log(`${index}:`, accumulator, element, arrayRef);
+	array,
+	(accumulator, element, index, arrayRef) => {
+		// console.log(`${index}:`, accumulator, element, arrayRef);
 
         return accumulator + element;
-    },
-    INITIAL_ACCUMULATOR,
+	},
+	INITIAL_ACCUMULATOR
 );
 
-console.log(result); // 21
+console.log(result); // 21 <- DONE!
 
-exports.reduce = reduce;
+// exports.reduce = reduce;
